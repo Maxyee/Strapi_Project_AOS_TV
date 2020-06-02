823e7cc716ac21ba51f9c3c262a19c15192d433c
maxyee <eyaminkhan00@gmail.com>
2020-06-02 09:53:49 +0600

    Added the details page compenent with html and bootstrap structure

--- NewsWebReact/src/pages/details/Details.js
+++ NewsWebReact/src/pages/details/Details.js
@@ -3,8 +3,42 @@ import React from 'react';
 
 function Details(){
     return(
-        <div>
-            Details page for project
+        <div className="container">
+            <div className="row">          
+                <div className="col-md-12">
+                    News heading
+                </div>
+            </div>
+            <div className="row">        
+                <div className="col-md-12">
+                    Time 
+                </div>
+            </div>
+            <div className="row">                
+                <div className="col-md-12">
+                    Atuhor name
+                </div>
+            </div>
+            <div className="row">         
+                <div className="col-md-12">
+                    image
+                </div>
+            </div>
+            <div className="row">   
+                <div className="col-md-12">
+                    description head
+                </div>
+            </div>
+            <div className="row">
+                <div className="col-md-12">
+                    news description
+                </div>
+            </div>
+            <div className="row">
+                <div className="col-md-12">
+                    country
+                </div>
+            </div>
         </div>
     );
 }
